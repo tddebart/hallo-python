381667da7e6fb79641d38922874fc21d70eb09cb
tddebart <tddebart@gmail.com>
2021-09-06 09:53:39 +0200

    added hello.py

---
+++ hello.py
@@ -0,0 +1,4 @@
+# Thomas de Bart
+# 99066756
+print("Hello wereld van...")
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn\n    p   p    y   y       t      h   h   o    o  n   n\n    p   p     y y        t      h   h   o    o  n   n\n    pppp       y         t      hhhhh   o    o  n   n\n    p          y         t      h   h   o    o  n   n\n    p          y         t      h   h   o    o  n   n\n    p          y         t      h   h    oooo   n   n")
